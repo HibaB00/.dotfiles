eb754c76d77a4ac8bfba50a98b6a93c39bbc4c07
HibaB00 <hibabayazid@gmail.com>
2024-08-27 16:31:56 -0400

    changes in notify

--- nvim/.config/nvim/lua/custom/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-notify.lua
@@ -2,14 +2,7 @@ return {
 	"rcarriga/nvim-notify",
 	opts = function(_, opts)
 		opts.stages = "fade"
-		opts.timeout = 3000
-		opts.max_height = 10
-		opts.max_width = 10
-		opts.on_open = nil
-		opts.on_close = nil
-		opts.render = "default"
-		opts.background_colour = "#000000"
-		opts.category = "info"
+		opts.timeout = 5000
 		opts.icons = {
 			ERROR = "",
 			WARN = "",
