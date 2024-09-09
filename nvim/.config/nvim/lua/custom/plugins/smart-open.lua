ec49e595dacaa9c1023c515baccd3c230f6405e5
HibaB00 <hibabayazid@gmail.com>
2024-09-09 15:30:03 -0400

    add codesnap

--- nvim/.config/nvim/lua/custom/plugins/smart-open.lua
+++ nvim/.config/nvim/lua/custom/plugins/smart-open.lua
@@ -7,8 +7,8 @@ return {
 	dependencies = {
 		"kkharji/sqlite.lua",
 		-- Only required if using match_algorithm fzf
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-		{ "nvim-telescope/telescope-fzy-native.nvim" },
+		-- { "nvim-telescope/telescope-fzy-native.nvim" },
 	},
 }
