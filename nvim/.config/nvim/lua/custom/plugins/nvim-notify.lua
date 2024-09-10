c5839a323ac90af6d5cfccfd499ad71cc4fb2fa2
HibaB00 <hibabayazid@gmail.com>
2024-09-10 10:54:20 -0400

    chnage animation to static

--- nvim/.config/nvim/lua/custom/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-notify.lua
@@ -1,8 +1,8 @@
 return {
 	"rcarriga/nvim-notify",
 	opts = function(_, opts)
-		opts.stages = "fade"
-		opts.timeout = 5000
+		opts.stages = "static"
+		opts.timeout = 1000
 		opts.icons = {
 			ERROR = "",
 			WARN = "",
