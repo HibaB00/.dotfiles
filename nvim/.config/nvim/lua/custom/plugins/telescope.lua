15fc0216dba647870f846627188bbe82adae35e5
HibaB00 <hibabayazid@gmail.com>
2024-12-30 16:05:13 -0500

    fix formatting

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-      -- Setup Telescope
-      telescope.setup {}
+			-- Setup Telescope
+			telescope.setup({})
 
-      -- Key mappings for Telescope
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find Files' })
-      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find Git Files' })
-      vim.keymap.set('n', '<leader>ps', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end, { desc = 'Grep for String' })
-    end
-  },
+			-- Key mappings for Telescope
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find Git Files" })
+			vim.keymap.set("n", "<leader>ps", function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end, { desc = "Grep for String" })
+		end,
+	},
 }
